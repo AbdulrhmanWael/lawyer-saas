52fdce278545693c1a8785be29ac55e18066db89
AbdulrhmanWael <abdelrhmanelkholy296@gmail.com>
2025-09-27 21:56:13 +0300

    build fix

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -46,14 +46,10 @@ import { AboutUsModule } from './about-us/about-us.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '5432'),
-      username: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASS || 'password',
-      database: process.env.DB_NAME || 'lawyer_db',
+      url: process.env.DB_URL,
       ssl: { rejectUnauthorized: false },
       autoLoadEntities: true,
-      synchronize: false,
+      synchronize: true,
     }),
     CacheModule.register({
       store: redisStore,
